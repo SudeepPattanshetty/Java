8baece50f2ae6cfa2eb6159d48faa9edfe614b08
Sudeep <93774921+SudeepPattanshetty@users.noreply.github.com>
2024-09-21 21:34:37 +0530

    Created To_do_list Project

---
+++ Projects/Basic/To_Do_List.java
@@ -0,0 +1,107 @@
+package Projects;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+class InnerTo_List{
+    Scanner in = new Scanner(System.in);
+
+    public void addTask(ArrayList<String> list)throws IOException{
+        System.out.println("Enter an item to add: ");
+        String item = in.next();
+        list.add(item);
+        saveTasksFile(list);
+        System.out.println("Item Added Successful");
+    }
+
+    public void viewTask(ArrayList<String> list){
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println("Tasks to complete");
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        if(list.isEmpty())
+            System.out.println("No Tasks Available");
+        else
+            System.out.println(list);
+    }
+
+    public void removeTask(ArrayList<String> list)throws IOException{
+        System.out.println("Enter an item to remove from the tasks: ");
+        String item = in.next();
+        if(list.remove(item)){
+            saveTasksFile(list);
+            System.out.println("Item Removed Successfully");
+        }
+        else
+            System.out.println("Item not found!");
+    }
+    
+    public void saveTasksFile(ArrayList<String> list) throws IOException {
+        FileWriter writer = new FileWriter("tasks.txt");
+        for(String task: list){
+            writer.write(task+ "\n");
+        }
+        writer.close();
+    }
+
+    public ArrayList<String> loadTasksFromFile() throws IOException{
+        ArrayList<String> list = new ArrayList<>();
+        File file = new File("tasks.txt");
+
+        if (file.exists()) {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String task;
+            while ((task = reader.readLine()) != null) {
+                list.add(task);
+            }
+            reader.close();
+        }
+        return list;
+    }
+}
+
+public class To_List {
+    public static void main(String[] args)throws IOException{
+        Scanner in = new Scanner(System.in);
+        InnerTo_List l = new InnerTo_List();
+        
+        System.out.println("Enter your name: ");
+        String name = in.next();
+        ArrayList<String> list = l.loadTasksFromFile();
+        
+
+        
+
+        while (true) {
+            
+            System.out.println("1] View Tasks");
+            System.out.println("2] Add Tasks");
+            System.out.println("3] Delete Tasks");
+            System.out.println("4] quit");
+            int ch = in.nextInt();
+            if(ch == 1){
+                l.viewTask(list);
+            }
+            else if(ch == 2){
+                l.addTask(list);
+            }
+            else if(ch == 3){
+                l.removeTask(list);
+            }
+            else if(ch == 4){
+                System.out.println("Successfully quit");
+                break;
+            }
+            else{
+                System.out.println("Invalid input");
+                break;
+            }
+            System.out.println("\n\n\n");
+        }
+    }   
+}
